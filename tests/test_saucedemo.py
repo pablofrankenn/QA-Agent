9c7d15463676dcf2a8d1406b5086dbb352cab2a0
Pablo Frankenberger <pablo.frankenn@gmail.com>
2026-05-17 01:28:39 -0300

    fix headless mode for ci environment

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -6,7 +6,7 @@ BASE_URL = "https://www.saucedemo.com"
 @pytest.fixture
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         yield page
         browser.close()
